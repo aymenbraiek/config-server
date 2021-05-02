1b0db7dbca3d6bcd4b663f474adacb19b55c08a6
aymenbraiek <aymen.braiek@esprit.tn>
2021-05-02 13:12:10 +0100

    Adding Externalized configuration closes 1

---
+++ src/test/java/com/biat/targa/labs/dev/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biat.targa.labs.dev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
